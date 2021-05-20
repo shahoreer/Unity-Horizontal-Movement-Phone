2cf9839e3b242ccb91a36328ee44f4eccf08e014
shahoreer <81192173+shahoreer@users.noreply.github.com>
2021-05-20 13:56:09 -0400

    Add files via upload

---
+++ player.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+
+    float xpos;
+    float ypos;
+    float half_scrn_x;
+
+    void Start()
+    {
+        half_scrn_x = Screen.width / 2;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            float xclickpos = Input.mousePosition.x;
+            if (xclickpos>half_scrn_x)
+            {
+                move(true);
+            }
+            else
+            {
+                move(false);
+            }
+        }
+
+    }
+
+
+    void move(bool right)
+    {
+        if (right)
+        {
+            xpos = transform.position.x + 0.5f;
+        }
+        else
+        {
+            xpos = transform.position.x - 0.5f;
+        }
+
+        ypos=transform.position.y;
+        xpos = Mathf.Clamp(xpos, -1.8f, 1.8f);
+        transform.position = new Vector3(xpos, ypos, 0);
+    }
+}
